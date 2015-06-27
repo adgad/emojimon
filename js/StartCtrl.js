c9d2962ad8c5e365c8937a616ee4a88830b1f2de
Arjun Gadhia <arjungadhia@gmail.com>
2015-06-27 07:43:00 +0100

    Test social

--- js/StartCtrl.js
+++ js/StartCtrl.js
@@ -1,5 +1,9 @@
 controllers
 
-.controller('StartCtrl', function($scope, $timeout, $compile, Game, $state) {
+.controller('StartCtrl', function($scope, $timeout, $compile, Game, $state, $cordovaSocialSharing) {
 		$scope.game = Game;
+
+	 $scope.shareAnywhere = function() {
+      $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
+  }
 });
