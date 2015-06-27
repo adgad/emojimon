e858bb7d335b280b50a5f325d305e2494110b55e
Arjun Gadhia <arjungadhia@gmail.com>
2015-06-27 12:37:54 +0100

    Watch to pause time, and highlight high score

--- www/js/emoji-factory.js
+++ www/js/emoji-factory.js
@@ -9,7 +9,7 @@ factories
 			onFall: {
 				addToScore: 1
 			},
-			probability: [50, 45, 30, 25],
+			probability: [40, 45, 30, 25],
 			fallDuration: {
 				min: 3.3,
 				max: 4.1
@@ -19,7 +19,7 @@ factories
 					top: element.scrollTop,
 					webkitTransform: 'scale(0.1) translate3d(0,0,0)',
 					transform: 'scale(0.1) translateY(0)',
-					transitionDuration: '0.7s'
+					transition: 'all 0.7s ease-in'
 				}
 			}
 		},
@@ -40,7 +40,7 @@ factories
 					top: element.scrollTop,
 					webkitTransform: 'scale(0.1) translate3d(0,0,0)',
 					transform: 'scale(0.1) translateY(0)',
-					transitionDuration: '0.7s'
+					transition: 'all 0.7s ease-in'
 				}
 			}
 		},
@@ -49,16 +49,16 @@ factories
 			onFall: {
 				endGame: 'Oh no, you let misery and despair in to your life...'
 			},
-			probability: [50, 45, 40, 30],
+			probability: [60, 45, 40, 30],
 			fallDuration: {
-				min: 3.2,
+				min: 2.5,
 				max: 3.5
 			},
 			endTransition: function(element){
 				return {
 					webkitTransform: 'rotate(180deg) scale(1.1)',
 					transform: 'rotate(180deg) scale(1.1)',
-					transitionDuration: '0.4s',
+					transition: 'all 0.4s ease-in',
 					opacity: 0
 				}
 			}
@@ -77,14 +77,14 @@ factories
 				return {
 					webkitTransform: 'rotate(180deg) scale(1.1)',
 					transform: 'rotate(180deg) scale(1.1)',
-					transitionDuration: '0.4s',
+					transition: 'all 0.4s ease-in',
 					opacity: 0
 				}
 			}
 		},
 		"bomb": {
 			onClick: {
-				addToScore: -3,
+				addToScore: -5,
 				removeSelector: 'emoji:not([type="bomb"])'
 			},
 			onFall: {},
@@ -98,7 +98,27 @@ factories
 					top: element.offsetTop,
 					webkitTransform: 'scale(3) rotate(1000deg) translate3d(0,0,0)',
 					transform: 'scale(3) rotate(1000deg) translated3(0,0,0)',
-					transitionDuration: '0.2s'
+					transition: 'all 0.7s ease-out'
+				}
+			}
+		},
+		"watch": {
+			onClick: {
+				addToScore: -3,
+				pause: true
+			},
+			onFall: {},
+			probability: [0, 0, 5, 8],
+			fallDuration: {
+				min: 2.3,
+				max: 4.1
+			},
+			endTransition: function(element){
+				return {
+					top: element.offsetTop,
+					webkitTransform: 'scale(2) rotate(1000deg) translate3d(0,0,0)',
+					transform: 'scale(2) rotate(1000deg) translated3(0,0,0)',
+					transition: 'all 0.7s ease-out'
 				}
 			}
 		}
