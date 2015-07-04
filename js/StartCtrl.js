e2bc8c9a52a534038102d07d54d024ab634afe78
Arjun Gadhia <arjungadhia@gmail.com>
2015-07-04 09:19:05 +0100

    Bug fixes

--- js/StartCtrl.js
+++ js/StartCtrl.js
@@ -6,9 +6,9 @@ controllers
  	$scope.shareAnywhere = function() {
  			if($scope.game.hasPlayed) {
 
-        $cordovaSocialSharing.share("I just scored " + $scope.game.score + " on Emojimon", null, null, device.platform == "Android" ? "https://play.google.com/store/apps/details?id=com.ionicframework.emojimon715922" : "https://play.google.com/store/apps/details?id=com.ionicframework.emojimon715922"); //TODO: switch for ios url
+        $cordovaSocialSharing.share("I just scored " + $scope.game.score + " on Emojimon", null, null, device.platform == "Android" ? "https://play.google.com/store/apps/details?id=com.ionicframework.emojimon715922" : "https://itunes.apple.com/us/app/emojimon/id1010959753?ls=1&mt=8"); //TODO: switch for ios url
  			} else {
- 				 $cordovaSocialSharing.share("Download Emojimon. It's awesome. Honest.", null, null, device.platform == "Android" ? "https://play.google.com/store/apps/details?id=com.ionicframework.emojimon715922" : "https://play.google.com/store/apps/details?id=com.ionicframework.emojimon715922"); //TODO: switch for ios url
+ 				 $cordovaSocialSharing.share("Download Emojimon. It's awesome. Honest.", null, null, device.platform == "Android" ? "https://play.google.com/store/apps/details?id=com.ionicframework.emojimon715922" : "https://itunes.apple.com/us/app/emojimon/id1010959753?ls=1&mt=8"); //TODO: switch for ios url
  			}
     }
 
