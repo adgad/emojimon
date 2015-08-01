59a636d040347227216fac50853a5ee1615f5655
Arjun Gadhia <arjungadhia@gmail.com>
2015-08-01 18:59:49 +0100

    Improve leaderboard

--- www/js/leaderboard-factory.js
+++ www/js/leaderboard-factory.js
@@ -2,6 +2,6 @@ factories
 .factory('Leaderboard', function($firebaseArray) {
 
   var scoresRef = new Firebase("https://emojimon.firebaseio.com/scores");
-  
-  return $firebaseArray(scoresRef.orderByPriority().limitToFirst(20));
+
+  return $firebaseArray(scoresRef.orderByPriority());
 });
